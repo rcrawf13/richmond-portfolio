f4af80ba2eaf3898d168ade9ce09899201102d33
rcrawf13 <richmondcrawford44@gmail.com>
2025-08-09 20:30:06 -0400

    Testing Mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css'
+import './index.css'
+import NavBar from './components/NavBar'
 
 function App() {
-  
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+      <>
+      <NavBar/>
+      <div className='Wrapper'>
+          <div className='box-frame'>
+            <div id='top-left'></div>
+
+            <div className='content'>
+              <h1>Brianna <br /> <u>Greenslade</u></h1>
+              <button>Lets Connect</button>
+            </div>
+            
+            <div className='frame'></div>
+          <div id='bottom-right'></div>
+
+          </div> 
+        </div>
+
+      </>
+          )
 }
 
 export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import "./navbar.css"
+
+const NavBar = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href="#"><u>Home</u></a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
